4b37ecf56123beb8d02742f162f6dac0f0d14988
Matt Feury <mattfeury@gmail.com>
2011-03-13 22:02:07 -0400

    Added library support to the repo

--- runTest.sh
+++ runTest.sh
@@ -1,2 +1,2 @@
-javac -d . src/*.java
-java KaossTest
+javac -cp lib/GlulogicMT.jar::lib/jsyn_beta_163.jar  -d . src/*.java
+java -cp lib/GlulogicMT.jar::lib/jsyn_beta_163.jar -Djava.library.path=lib/ KaossTest
