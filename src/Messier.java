202eddd67c4e093edb3b37e4ec8d3ded4fc7f323
Mike Hirth <hirthy@gmail.com>
2011-03-27 21:25:39 -0400

    Change Messier

--- src/Messier.java
+++ src/Messier.java
@@ -6,46 +6,52 @@ import com.softsynth.jsyn.*;
 
 public class Messier extends Instrument {
 
-    public Messier()
+    
+    public Messier(Instrument... extras)
     {
         super();
 
         //set characteristics
         scale = majorScale;
-		harmonics = oddHarmonics; //Add triangle harmonics and noise
+		    harmonics = new int[]{}; 
 
+        for(Instrument i : extras)
+          extraneous.add(i);    
+        
         //make timbre and start        
         makeTimbre();
-        startScope();        
+        startScope();    
     }
     
     public void makeTimbre()
     {
-        mixer = new SynthMixer(harmonics.length + 1, 2);      
-        for(int i = 0; i < harmonics.length; i++)
-          {
-            SineOscillator sineOsc = new SineOscillator();
-            sineInputs.add(sineOsc);
-            freqMods.add(sineOsc.frequency);
+       mixer = new SynthMixer(harmonics.length + extraneous.size(), 2);  
+       //this can go away since there are no custom harmonics. i'll leave it here in case you want to play around with more
+       for(int i = 0; i < harmonics.length; i++)
+       {
+         SineOscillator sineOsc = new SineOscillator();
+         sineInputs.add(sineOsc);
+         freqMods.add(sineOsc.frequency);
 
-            //stereo wavves
-            mixer.connectInput( i, sineOsc.output, 0 );
-            mixer.setGain( i, 0, amplitude );
-            mixer.setGain( i, 1, amplitude );
+         //stereo wavves
+         mixer.connectInput( i, sineOsc.output, 0 );
+         mixer.setGain( i, 0, amplitude );
+         mixer.setGain( i, 1, amplitude );
 
-            sineOsc.amplitude.set(amplitude / Math.pow(i+1,2));; //Set amplitude for triangle harmonics 
-          }
-		//Add noise
-		com.softsynth.jsyn.RedNoise noiseOsc = new com.softsynth.jsyn.RedNoise();
-		sineInputs.add(noiseOsc);
-        freqMods.add(noiseOsc.frequency);
+         sineOsc.amplitude.set(amplitude); 
+       }
 
-        //stereo wavves
-        mixer.connectInput(harmonics.length, noiseOsc.output, 0 );
-        mixer.setGain( harmonics.length, 0, amplitude );
-        mixer.setGain( harmonics.length, 1, amplitude );
-
-        noiseOsc.amplitude.set(amplitude);  //noise
+       int i =0;
+       for(Instrument extra : extraneous)
+       {
+         SynthMixer extraMixer = extra.getMixer();
+         mixer.connectInput( harmonics.length + i, extraMixer.getOutput(0), 0);
+         mixer.setGain( harmonics.length + i, 0, amplitude * 2.0 / 3.0 );
+         mixer.setGain( harmonics.length + i, 1, amplitude * 2.0 / 3.0 );
+         extraMixer.start();
+         i++;
+       }
+        
     }
     
     public void start()  
@@ -53,9 +59,12 @@ public class Messier extends Instrument {
        System.out.println("start");
        isPlaying = true;
 
-       envPlayer.envelopePort.clear(); // clear the queue        
+       envPlayer.envelopePort.clear(); // clear the queue         
        envPlayer.envelopePort.queueLoop(envData );  // queue an envelope
-       
+
+       for(Instrument extra : extraneous)
+         extra.start();
+
        for(SynthOscillator sineOsc : sineInputs)
          sineOsc.start();
     }
@@ -64,20 +73,29 @@ public class Messier extends Instrument {
     {
        System.out.println("stop");
        isPlaying = false;
+
+       for(Instrument extra : extraneous)       
+         extra.stop();
+
        for(SynthOscillator sineOsc : sineInputs)
          sineOsc.stop();
     }
     
     public void adjustFrequencyByOffset(int offset) {
-		for(int i=0; i < harmonics.length; i++) 
-		{
-			double scaleOffset = getScaleIntervalFromOffset(scale, (int)offset + harmonics[i]);
-			int freq = (int)(Math.pow(2,((scaleOffset) / 12)) * BASE_FREQ);
-			
-			freqMods.get(i).set(freq * harmonics[i]);
-		}
+        //harmonic mode
+        for(Instrument extra : extraneous)       
+          extra.adjustFrequencyByOffset(offset);
 
-
+        int i = 0;
+        double scaleOffset = getScaleIntervalFromOffset(scale, offset);    
+        int freq = (int)(Math.pow(2,((scaleOffset) / 12)) * BASE_FREQ);
+        
+        for(SynthInput freqMod : freqMods)
+        {
+            //harmonic offset
+            freqMod.set(freq * harmonics[i]);
+            i++;
+        }
     }   
     
     
